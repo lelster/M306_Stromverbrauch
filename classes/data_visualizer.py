d4e4b07bb8b3bb9d8add4d5ec4fe84975381c50f
moccil-bzz <moccil@bzz.com>
2024-10-01 11:45:53 +0200

    data processor matching data

--- classes/data_visualizer.py
+++ classes/data_visualizer.py
@@ -9,6 +9,7 @@ class DataVisualizer:
     def generate_consumption_chart(data: list[ConsumptionData], chart_type: str) -> object:
         """Docstring"""
 
+
     @staticmethod
     def generate_meter_readings_chart(data: list[MeterData], chart_type: str) -> object:
         """Docstring"""
