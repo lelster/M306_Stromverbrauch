8bf2d655aee8d4e33143a246b062a2df14c34288
moccil-bzz <moccil@bzz.com>
2024-10-02 09:04:48 +0200

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from classes.file_reader import FileReader
 from classes.data_processor import DataProcessor
 from classes.data_visualizer import DataVisualizer
+from classes.consumtion_data import ConsumptionData, ConsumptionEntry
 from classes.exporter import Exporter
 from classes.gui import Gui
 
@@ -9,6 +10,10 @@ def main():
     """
     Main Workflow
     """
+    reader = FileReader()
+    data: list[ConsumptionData] = reader.read_sdat_files("C:\\Users\\Leonardo Mocci\\Desktop\\XML-Files\\SDAT-Files")
+    for i in data:
+        print(i)
 
 if __name__ == "__main__":
     main()
