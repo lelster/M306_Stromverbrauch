ff2e77ea0ab92dfa1dedae37066df3f19f8108cd
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-10-02 19:42:41 +0200

    adjusted main by using the choice of the gui to call export method if needed. Added path from gui.py to save files

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def read() -> (list[ConsumptionData], list[MeterData]):
     return dataConsumption, dataMeter
 
 
-def export(export_type: str, dataConsumption: list[ConsumptionData], dataMeter: list[MeterData]):
+def export(path: str, export_type: str, dataConsumption: list[ConsumptionData], dataMeter: list[MeterData]):
     exporter = Exporter()
     if export_type == 'csv':
-        exporter.export_to_csv("./data/public/csv", dataConsumption, dataMeter)
+        exporter.export_to_csv(path, dataConsumption, dataMeter)
         print("csv exported")
     elif export_type == 'json':
-        exporter.export_to_json("./data/public/json", dataConsumption, dataMeter)
+        exporter.export_to_json(path, dataConsumption, dataMeter)
         print("json exported")
     else:
         print(f"Export type {export_type} is not supported.")
@@ -31,12 +31,13 @@ def export(export_type: str, dataConsumption: list[ConsumptionData], dataMeter: 
 def main():
     print("reading data...")
     dataConsumption, dataMeter = read()
-
-    print("What file format would you like to export? Options: csv, json")
-    export_type = input("Enter export type: ").lower()
-
-    export(export_type, dataConsumption, dataMeter)
-
+    gui = Gui()
+    if gui.choice == 'visualise':
+        pass
+    elif gui.choice == 'export':
+        path = str(gui.filedialog)
+        export_type = str(gui.export_format).lower()
+        export(path, export_type, dataConsumption, dataMeter)
 
 if __name__ == "__main__":
     main()
