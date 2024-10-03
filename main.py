f8d7d9065ed90311e0e9f17631c9d2def5c22da2
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-10-03 11:00:45 +0200

    moved export to gui.py

--- main.py
+++ main.py
@@ -15,29 +15,15 @@ def read() -> (list[ConsumptionData], list[MeterData]):
     return dataConsumption, dataMeter
 
 
-def export(path: str, export_type: str, dataConsumption: list[ConsumptionData], dataMeter: list[MeterData]):
-    exporter = Exporter()
-    if export_type == 'csv':
-        exporter.export_to_csv(path, dataConsumption, dataMeter)
-        print("csv exported")
-    elif export_type == 'json':
-        exporter.export_to_json(path, dataConsumption, dataMeter)
-        print("json exported")
-    else:
-        print(f"Export type {export_type} is not supported.")
+
 
 
 
 def main():
     print("reading data...")
     dataConsumption, dataMeter = read()
-    gui = Gui()
-    if gui.choice == 'visualise':
-        pass
-    elif gui.choice == 'export':
-        path = str(gui.filedialog)
-        export_type = str(gui.export_format).lower()
-        export(path, export_type, dataConsumption, dataMeter)
+    Gui(dataConsumption, dataMeter)
+
 
 if __name__ == "__main__":
     main()
