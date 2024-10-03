7ba82ede639e90402df5279e61a1f86b66052559
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-10-03 10:59:51 +0200

    moved export to gui.py to keep gui open after choosing to export.

--- classes/gui.py
+++ classes/gui.py
@@ -1,36 +1,50 @@
 import customtkinter as ctk
+from tkinter import filedialog
+import os
+from classes.consumtion_data import ConsumptionData
+from classes.exporter import Exporter
+from classes.meter_data import MeterData
 
-class Gui:
+
+class Gui():
     """GUI zur Auswahl von Visualisierung oder Export mit Formatwahl"""
 
-    def __init__(self):
+    def __init__(self, dataConsumption, dataMeter):
+        self.back_button = None
+        self.esl_button = None
+        self.sdat_button = None
+        self.dataConsumption = dataConsumption
+        self.dataMeter = dataMeter
         ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("green")
 
         self.root = ctk.CTk()
         self.root.title("Export und Visualisierung")
         self.root.geometry("400x300")
+        self.root.eval('tk::PlaceWindow . center')
 
         self.choice = None
         self.export_format = None
-        self.filedialog = ctk.filedialog.askdirectory
+        self.filedialog = None
 
         self.visualise_button = ctk.CTkButton(self.root, text='Visualisieren', command=self.choose_visualise)
-        self.visualise_button.pack(pady=(70, 10))
+        self.visualise_button.pack(pady=(50, 10))
+
+        self.add_files_button = ctk.CTkButton(self.root, text='Add Files', command=self.show_add_files_options)
+        self.add_files_button.pack(pady=(10, 10))
 
         self.export_button = ctk.CTkButton(self.root, text='Exportieren', command=self.choose_export)
         self.export_button.pack(pady=(10, 10))
 
-        self.label = ctk.CTkLabel(self.root, text="Exportformat wählen:")
-        self.label.pack(pady=(10, 5))
+
 
         self.export_format_var = ctk.StringVar(value="csv")
 
-        self.csv_radio = ctk.CTkRadioButton(self.root, text='CSV', variable=self.export_format_var, value='csv')
-        self.csv_radio.pack(pady=5)
+        self.label = ctk.CTkLabel(self.root, text="Exportformat wählen:")
+        self.label.pack(pady=(10, 5))
 
-        self.json_radio = ctk.CTkRadioButton(self.root, text='JSON', variable=self.export_format_var, value='json')
-        self.json_radio.pack(pady=5)
+        self.format_dropdown = ctk.CTkComboBox(self.root, state="readonly", values=["csv", "json"], variable=self.export_format_var)
+        self.format_dropdown.pack(pady=5)
 
         self.root.mainloop()
 
@@ -40,6 +54,59 @@ class Gui:
 
     def choose_export(self):
         self.choice = 'export'
-        self.filedialog = self.filedialog()
+        self.filedialog = ctk.filedialog.askdirectory()
         self.export_format = self.export_format_var.get()
-        self.root.destroy()
+        self.export(self.filedialog, self.export_format, self.dataConsumption, self.dataMeter)
+
+    def show_add_files_options(self):
+        self.visualise_button.pack_forget()
+        self.export_button.pack_forget()
+        self.label.pack_forget()
+        self.format_dropdown.pack_forget()
+        self.add_files_button.pack_forget()
+        self.esl_button = ctk.CTkButton(self.root, text="New ESL-File", command=self.add_new_esl_file)
+        self.esl_button.pack(pady=(100, 10))
+        self.sdat_button = ctk.CTkButton(self.root, text="New SDAT-File", command=self.add_new_sdat_file)
+        self.sdat_button.pack(pady=10)
+        self.back_button = ctk.CTkButton(self.root, text="<", width=30, height=30, corner_radius=10,
+                                    fg_color="white", text_color="black", command=self.show_buttons)
+        self.back_button.place(x=10, y=10)
+
+    def add_new_sdat_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            destination = 'data/public/SDAT-Files'
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+            os.rename(file_path, os.path.join(destination, os.path.basename(file_path)))
+            self.show_buttons()
+
+    def add_new_esl_file(self):
+        file_path = filedialog.askopenfilename()
+        if file_path:
+            destination = 'data/public/ESL-Files'
+            if not os.path.exists(destination):
+                os.makedirs(destination)
+            os.rename(file_path, os.path.join(destination, os.path.basename(file_path)))
+            self.show_buttons()
+
+    def show_buttons(self):
+        self.visualise_button.pack(pady=(50, 10))
+        self.add_files_button.pack(pady=(10, 10))
+        self.export_button.pack(pady=(10,10))
+        self.label.pack(pady=(10, 5))
+        self.format_dropdown.pack(pady=5)
+        self.sdat_button.pack_forget()
+        self.esl_button.pack_forget()
+        self.back_button.place_forget()
+
+    def export(self, path: str, export_type: str, dataConsumption: list[ConsumptionData], dataMeter: list[MeterData]):
+        exporter = Exporter()
+        if export_type == 'csv':
+            exporter.export_to_csv(path, dataConsumption, dataMeter)
+            print("csv exported")
+        elif export_type == 'json':
+            exporter.export_to_json(path, dataConsumption, dataMeter)
+            print("json exported")
+        else:
+            print(f"Export type {export_type} is not supported.")
