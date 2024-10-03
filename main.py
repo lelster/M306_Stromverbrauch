068f22a88caf6953f7403050833679d791ad6adb
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-10-03 15:38:33 +0200

    moved main()

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
+# main.py
 """Main File with Workflow"""
 from classes.file_reader import FileReader
 from classes.consumtion_data import ConsumptionData
 from classes.gui import Gui
 from classes.meter_data import MeterData
 
-def main():
-    print("Reading data...")
-    dataConsumption, dataMeter = read()
-    gui = Gui(dataConsumption, dataMeter)
-
 def read() -> (list[ConsumptionData], list[MeterData]):
     reader = FileReader()
     dataConsumption: list[ConsumptionData] = reader.read_sdat_files("./data/public/SDAT-Files")
     dataMeter: list[MeterData] = reader.read_esl_files("./data/public/ESL-Files")
     return dataConsumption, dataMeter
+
+def main():
+    print("Reading data...")
+    dataConsumption, dataMeter = read()
+    Gui(dataConsumption, dataMeter)
 
 if __name__ == "__main__":
     main()
